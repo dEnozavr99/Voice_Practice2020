1219683dd0a198ef3b5b7c5c5238e020cb90300d
dEnozavr99 <rudykdenys@gmail.com>
2020-01-16 21:32:50 +0200

    добавив Фур'є

--- Voice_analyzer/Form1.cs
+++ Voice_analyzer/Form1.cs
@@ -19,6 +19,8 @@ namespace Voice_analyzer
 {
     public partial class Form1 : Form
     {
+
+        double TwoPi = Math.PI * 2;
         //recording thread
         WaveIn waveIn;
         //record to file class
@@ -32,7 +34,7 @@ namespace Voice_analyzer
 
         string beepSoundFileName = @"C:\Users\Admin\Desktop\Learning\Pract\Voice\Voice_analyzer\Voice_analyzer\bin\Debug\beep.wav";
 
-        List<float> list = new List<float>();
+        List<double> list = new List<double>();
         List<short> list1 = new List<short>();
 
         int it = 0;
@@ -161,6 +163,7 @@ namespace Voice_analyzer
         {
             
             chart1.Series["Series1"].Points.Clear();
+            toolStripStatusLabel1.Text = list.Count.ToString();
             foreach (var i in list)
             {
                 chart1.Series["Series1"].Points.AddY(i);
@@ -175,9 +178,114 @@ namespace Voice_analyzer
 
             Player = new SoundPlayer(outputFileName);
             Player.Play();
-
-
         }
 
+        
+
+        void FFTAnalysis(double[] AVal, double[] FTvl, int Nvl, int Nft)
+        {
+            int i, j, n, m, Mmax, Istp;
+            double Tmpr, Tmpi, Wtmp, Theta;
+            double Wpr, Wpi, Wr, Wi;
+            double[] Tmvl;
+
+            n = Nvl * 2; Tmvl = new double[n];
+
+            for (i = 0; i < n; i += 2)
+            {
+                Tmvl[i] = 0;
+                Tmvl[i + 1] = AVal[i / 2];
+            }
+
+            i = 1; j = 1;
+            while (i < n)
+            {
+                if (j > i)
+                {
+                    Tmpr = Tmvl[i]; Tmvl[i] = Tmvl[j]; Tmvl[j] = Tmpr;
+                    Tmpr = Tmvl[i + 1]; Tmvl[i + 1] = Tmvl[j + 1]; Tmvl[j + 1] = Tmpr;
+                }
+                i = i + 2; m = Nvl;
+                while ((m >= 2) && (j > m))
+                {
+                    j = j - m; m = m >> 1;
+                }
+                j = j + m;
+            }
+
+            Mmax = 2;
+            while (n > Mmax)
+            {
+                Theta = -TwoPi / Mmax; Wpi = Math.Sin(Theta);
+                Wtmp = Math.Sin(Theta / 2); Wpr = Wtmp * Wtmp * 2;
+                Istp = Mmax * 2; Wr = 1; Wi = 0; m = 1;
+
+                while (m < Mmax)
+                {
+                    i = m; m = m + 2; Tmpr = Wr; Tmpi = Wi;
+                    Wr = Wr - Tmpr * Wpr - Tmpi * Wpi;
+                    Wi = Wi + Tmpr * Wpi - Tmpi * Wpr;
+
+                    while (i < n)
+                    {
+                        j = i + Mmax;
+                        Tmpr = Wr * Tmvl[j] - Wi * Tmvl[j - 1];
+                        Tmpi = Wi * Tmvl[j] + Wr * Tmvl[j - 1];
+
+                        Tmvl[j] = Tmvl[i] - Tmpr; Tmvl[j - 1] = Tmvl[i - 1] - Tmpi;
+                        Tmvl[i] = Tmvl[i] + Tmpr; Tmvl[i - 1] = Tmvl[i - 1] + Tmpi;
+                        i = i + Istp;
+                    }
+                }
+
+                Mmax = Istp;
+            }
+
+            for (i = 0; i < Nft; i++)
+            {
+                j = i * 2; FTvl[i] = 2 * Math.Sqrt(Math.Pow(Tmvl[j], 2) + Math.Pow(Tmvl[j + 1], 2)) / Nvl;
+            }
+        }
+
+        private int nextPowerOf2(int n)
+        {
+            int count = 0;
+
+            // First n in the below  
+            // condition is for the 
+            // case where n is 0 
+            if (n > 0 && (n & (n - 1)) == 0)
+                return n;
+
+            while (n != 0)
+            {
+                n >>= 1;
+                count += 1;
+            }
+
+            return 1 << count;
+        }
+
+        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
+        {
+            chart1.Series["Series1"].Points.Clear();
+
+            int arrayLenth = nextPowerOf2(list.Count);
+
+            double[] FurieOut = new double[arrayLenth];
+
+            for (int i = list.Count - 1; i <= arrayLenth - 1; i++)
+            {
+                list.Add(0);
+            }
+            
+
+            FFTAnalysis(list.ToArray(), FurieOut, list.Count - 1, FurieOut.Length - 1);
+            
+            for (int i = 0; i <= (FurieOut.Length - 1) / 2; i++)
+            {
+                chart1.Series["Series1"].Points.AddY(FurieOut[i]);
+            }
+        }
     }
 }
